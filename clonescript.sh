a58fe4a3253da76b2652fc35cc8e16041cbcbb4c
Thomas Chau <Thomas Chau>
2025-05-20 21:29:44 -0400

    Created a script that will delete and then clone Git repo for personal use

---
+++ clonescript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is a script that will remove the Git repo and then clone the repo to the local machine
+
+# Remove current Git repo on local machine
+sudo rm -r COMP2137
+
+# Clone Git repo to local machine
+git clone git@github.com:UziYim/COMP2137
+
